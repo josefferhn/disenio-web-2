4ec7aad34c3696766e59802fd41ecdfdb78420d0
Fernando Ramos <josefferhn@yahoo.com>
2017-11-16 09:08:46 -0600

    primer commit

---
+++ js/my-app.js
@@ -0,0 +1,34 @@
+// Initialize your app
+var myApp = new Framework7({
+	// Default title for modals
+    modalTitle: 'Framework7',
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+    swipePanel: 'left',
+	material: true
+    // ... other parameters
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+// Add another view, which is in right panel
+var rightView = myApp.addView('.view-right', {
+    name: 'right'
+});
+
+
+// Callbacks to run specific code for specific pages, for example for About page:
+myApp.onPageInit('about', function (page) {
+    // run createContentPage func after link was clicked
+    $$('.create-page').on('click', function () {
+        createContentPage();
+    });
+});
+
